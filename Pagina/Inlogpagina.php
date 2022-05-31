416a893bf594483ca4474fa9e771d35fff81fe10
Timhamer <240159@edu.rocfriesepoort.nl>
2022-05-31 13:41:45 +0200

    Inlog poging nummer 3

--- Pagina/Inlogpagina.php
+++ Pagina/Inlogpagina.php
@@ -20,11 +20,13 @@
             <div class="InlogBlokLinks"></div>
             <div class="InlogBlokRechts"></div> 
             <div><h2>Inloggen</h2></div>
+            <form method="POST" action="#">
             <div class="InlogInputDiv"><input class="InlogInput" type="tekst" placeholder="E-mail" name="Email" required></div>
             <div class="InlogInputDiv"><input class="InlogInput" type="password" placeholder="Wachtwoord" name="Wachtwoord" required></div>
             <div></div>
-            <div><center><button name="Login" onclick="window.location.href='Index.php'" type="submit" class="InlogButton">Log in</button></center></div>
-        </div>
+            <div><center><button name="Login" onclick="window.location.href='Bestellingenoverzicht.php'" type="submit" class="InlogButton">Log in</button></center></div>
+            
+        </div></form>
 
     </div>
 
@@ -33,4 +35,26 @@
 </html>
 
 <?php
-if (isset())
+session_start();
+include("Connection.php");
+include("Functions.php");
+
+//$username = check_login($conn);
+
+if(isset($_POST['Email']))
+    {
+        $mail = $_POST['Email'];
+        $wachtwoord = $_POST['Wachtwoord'];
+
+        $sql = "select * from users where email = '".$mail."'AND password = '".$wachtwoord."' limit 1";
+
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) == 1)
+        {
+          echo "You logged in";
+        }
+        else {
+          echo "You have entered incorrect email or username";
+        }
+    }
