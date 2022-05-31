312281a74eb2f0af80e5a6b85c0f16493389f3bf
Timhamer <240159@edu.rocfriesepoort.nl>
2022-05-31 10:06:41 +0200

    Bootstrap proberen

--- Pagina/Inlogpagina.php
+++ Pagina/Inlogpagina.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/Styles.css">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js%22%3E"></script>
     <title>Document</title>
 </head>
 <body>
@@ -16,13 +19,18 @@
         <div class="InlogGrid">
             <div class="InlogBlokLinks"></div>
             <div class="InlogBlokRechts"></div> 
-            <div><center><h2>Inloggen</h2></center></div>
-            <div><input class="InlogInput" type="tekst" placeholder="E-mail" name="Email" required></div>
-            <div><input class="InlogInput" type="password" placeholder="Wachtwoord" name="Wachtwoord" required></div>
+            <div><h2>Inloggen</h2></div>
+            <div class="InlogInputDiv"><input class="InlogInput" type="tekst" placeholder="E-mail" name="Email" required></div>
+            <div class="InlogInputDiv"><input class="InlogInput" type="password" placeholder="Wachtwoord" name="Wachtwoord" required></div>
             <div></div>
-            <div><center><button name="Login" onclick="window.location.href='Index.php'" type="submit" class="LoginButton">Log in</button></center></div>
+            <div><center><button name="Login" onclick="window.location.href='Index.php'" type="submit" class="InlogButton">Log in</button></center></div>
         </div>
 
     </div>
+
+
 </body>
 </html>
+
+<?php
+if (isset())
