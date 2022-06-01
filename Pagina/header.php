916d2987b0bae3c4abd85a6c0a6b8c45e9f4a733
nitronukes <89461502+nitronukes@users.noreply.github.com>
2022-06-01 08:57:32 +0200

    header
    dit is de header

---
+++ Pagina/header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>header</title>
+</head>
+<body>  
+   <div class="header">
+    <ul>
+  <li><a href="Inlogpagina.php">login</a></li>
+  <li><a href="Besteloverzicht.php">Besteloverzicht</a></li>
+  <li><a href=""></a></li>
+  <li><a href=""></a></li>
+</ul>
+</div>
+</body>
+</html>
+
