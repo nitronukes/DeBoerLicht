39e3bee67f239265faeeff27b06ef2862c07f814
nitronukes <89461502+nitronukes@users.noreply.github.com>
2022-06-08 10:32:41 +0200

    Update header.php

--- Pagina/header.php
+++ Pagina/header.php
@@ -5,20 +5,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/Styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>  
     <header>
 
     <div class="menu">
          <ul>
+
              <img class="logo" src="../Fotos/logo_timothy.png"/>
+             <li><a class="fa fa-shopping-cart" style="font-size:24px"></a></li>
             <li><a href="Inlogpagina.php">login</a></li>
             <li><a href="Bestellingenoverzicht.php">Besteloverzicht</a></li>
-            <!--<li><a href=""></a></li>-->
-            <li><a href=""><i class="fa-solid fa-cart-shopping" style="background-color:black; height: 50px; width: 50px;"></i></a></li>
+
+
+
+
         </ul>
+
+
     </div>
+
+
+
     </header>
 </body>
 </html>
