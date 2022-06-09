b5bb29edc2b6e9ec9a2568c0c8b617e0d1f8f27e
nitronukes <89461502+nitronukes@users.noreply.github.com>
2022-06-09 13:06:05 +0200

    winkel wagen begin

--- Pagina/winkelwagen.php
+++ Pagina/winkelwagen.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +8,60 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<body>
+<body class="winkelwagen">
 <div class="wrapper">
     <h1>winkelwagen</h1>
     <div class = "project">
     <div class = "shop">
     <div class = "box">
-
+        <img src="../Fotos/logo_timothy.png" alt ="">
+        <h3>product 1</h3>
+        <h4>Price: Your mom</h4>
+        <p class="unit">hoeveelheid: <input value="2"></p>
+        <p class="btn-area"></p>
+            <i class="fa fa-trash"</i>
+               <span class="btn2">verwijderen</span>
+        </p>
 </div>
 </div>
+</div>
+</div>
+<div class = "project">
+    <div class = "shop">
+        <div class = "box">
+            <img src="../Fotos/logo_timothy.png" alt ="">
+            <h3>product 1</h3>
+            <h4>Price: Your mom</h4>
+            <p class="unit">hoeveelheid: <input value="1"></p>
+            <p class="btn-area"></p>
+                <i class="fa fa-trash"</i>
+                <span class="btn2">verwijderen</span>
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+<div class = "project">
+    <div class = "shop">
+        <div class = "box">
+            <img src="../Fotos/logo_timothy.png" alt ="">
+            <h3>product 1</h3>
+            <h4>Price: Your mom</h4>
+            <p class="unit">hoeveelheid: <input value="0"</p>
+            <p class="btn-area"></p>
+            <i class="fa fa-trash"</i>
+                <span class="btn2">verwijderen</span>
+            </p>
+        </div>
+    </div>
+    <div class="right-bar"></div>
+            <p><span>Subtotaal</span> <span>120euro</span> </p>
+    <hr>
+            <p><span>Tax (5%)</span>  <span>6euro</span></p>
+    <hr>
+            <p><span>shipping</span>  <span>15euro</span></p>
+    <hr>
+            <p><span>Totaal</span>  <span>141euro</span></p>
 </div>
 </div>
 </body>
