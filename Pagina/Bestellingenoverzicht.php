4d6a0badca8dd150a7100ce70bc11fe234986171
Christianfk2905 <c.f.koopman2905@gmail.com>
2022-06-22 11:00:08 +0200

    yeet

--- Pagina/Bestellingenoverzicht.php
+++ Pagina/Bestellingenoverzicht.php
@@ -1,116 +1,89 @@
 <?php
-include 'connection.php';
-include 'header.php';
-//
-$sql=" SELECT * FROM bestelling";
-$result = $conn->query($sql);
+    session_start();
+    require 'Connection.php';
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/Styles.css">
-  <title>Bestellingenoverzicht</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Bestellingenoverzicht</title>
 </head>
-
-
-  
-</style>
-<header>
-<form method='POST' action=""> 
-</form> 
-    
-
-
 <body>
-<br><br><br><br>
-
-        <tr>
-        <div class='table-wrapper'>
-         <table class='fl-table'>
-        <thead>
-            <th>Naam</th>
-            <th>datum</th>
-            <th>tijd</th>
-            <th>prijs</th>
-            <th>accepteer</th>
-            <th>weiger</th>
-            </tr>  
-        </thead>
-        <tbody>
-<?php
-$naam=$_SESSION['username'];
-echo $naam;
-
-if(isset($_POST['loguit'])){
-  session_destroy();
-  header('Location: ');
-}
-error_reporting(0);
-if(isset($_SESSION['admin_name'])){
-
-$query= "SELECT * FROM bestelling";
-
- } elseif (isset($_SESSION['username'])){
-  $naam=$_SESSION['username'];
-
-  echo $naam;
-
-  $query= "SELECT * FROM `bestelling` WHERE `naam` = '.$naam.'";
-}
-
-$data = mysqli_query($conn,$query);
-
-$total = mysqli_num_rows($data);
-
-if($total!=0){
-  while($result=mysqli_fetch_assoc($data)){
-      echo "
-      <tr>
-      
-      <td>".$result['naam']."</td>
-      <td>".$result['datum']."</td>
-      <td>".$result['tijd']."</td>
-      <td>".'€'.$result['totaal']."</td>
-      <tbody>
-      </tr>
-      ";
-  }
-    } ?>
- 
-  </table>
-
   
+    <div class="container mt-4">
 
-<?php
- $id= $_GET['id'];
-$sql= "SELECT * FROM bestelling where id=$id";
-$res = $conn->query($sql);
-if ($res) {
-  foreach ($res as $result) {
-echo"
-<p class='naam'>".$result['naam']."</p>
-<p class='tijd'>afhaaltijdstip: ".$result['tijd']."</p>
-<br>
-<p class='datum'>".$result['datum']."</p>
-<br>
-<p>".$result['name']."</p>
-<br><br><br>
-<p>".'€'.$result['totaal']."</p>
+        <?php include('message.php'); ?>
 
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Student Details
+                            <a href="Bestelpagina.php" class="btn btn-primary float-end">Geaccepteerde bestellingen</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
 
-";}} ?>
-      
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Voledige naam</th>
+                                    <th>Email</th>
+                                    <th>Telefoonnummer</th>
+                                    <th>Adress</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $query = "SELECT * FROM bestellingen";
+                                    $query_run = mysqli_query($conn, $query);
 
-    <a type='button' class='sluitknop' href='/DeBoerLicht/Pagina/Bestellingenoverzicht.php'>&times;</a>
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    {
+                                        foreach($query_run as $student)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?= $student['id']; ?></td>
+                                                <td><?= $student['name']; ?></td>
+                                                <td><?= $student['email']; ?></td>
+                                                <td><?= $student['phone']; ?></td>
+                                                <td><?= $student['adress']; ?></td>
+                                                <td>
+                                                    
+                                                    <a href="student-edit.php?id=<?= $student['id']; ?>" class="btn btn-success btn-sm">Accepteren</a>
+                                                    <form action="code.php" method="POST" class="d-inline">
+                                                        <button type="submit" name="delete_student" value="<?=$student['id'];?>" class="btn btn-danger btn-sm">Verwijder</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        echo "<h5> No Record Found </h5>";
+                                    }
+                                ?>
+                                
+                            </tbody>
+                        </table>
 
-</div>
-  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
-
 </html>
