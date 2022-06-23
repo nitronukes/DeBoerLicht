76ccb67132a2cf8302b4e31d903d6835b6b977bb
nitronukes <89461502+nitronukes@users.noreply.github.com>
2022-06-23 13:20:42 +0200

    werkende winkelwagen

--- Pagina/winkelwagenn.php
+++ Pagina/winkelwagenn.php
@@ -1,9 +1,9 @@
 <?php
-//include 'Connection.php';
+
 include 'header.php';
-session_start();
+
 $product_ids = array();
-session_destroy();
+//session_destroy();
 //kijken of product naar winkelwagen is verstuurd
 if(filter_input(INPUT_POST, 'add_to_cart')){
     if(isset($_SESSION['winkelwagen'])){
@@ -12,8 +12,6 @@ if(filter_input(INPUT_POST, 'add_to_cart')){
             $count = count($_SESSION['winkelwagen']);
 
         //maak een sequantial array om array keys aan products ids vast te maken
-
-
             $product_ids = array_column($_SESSION['winkelwagen'], 'id');
 
         if(!in_array(filter_input(INPUT_GET,'id' ), $product_ids)){
@@ -47,9 +45,21 @@ if(filter_input(INPUT_POST, 'add_to_cart')){
               'hoeveelheid'=> filter_input(INPUT_POST, 'hoeveelheid')
         );
     }
-    pre_r($_SESSION);
-}
+    }
+    if(filter_input(INPUT_GET, 'action')== 'delete'){
+        //loop door al de producten heen todat het gelijk is aan get id variabelen
+        foreach ($_SESSION['winkelwagen'] as $key => $product){
+            if($product['id']== filter_input(INPUT_GET, 'id')){
+               //verwijder product uit winkelwagen
+                unset($_SESSION['winkelwagen'][$key]);
+            }
+        }
+        //reset session array keys zodat ze gelijk zijn aan $product_ids numeric array
+        $_SESSION['winkelwagen'] = array_values($_SESSION['winkelwagen']);
 
+
+}
+pre_r($_SESSION);
 
 function pre_r($array){
     echo'<pre>';
@@ -66,30 +76,30 @@ function pre_r($array){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
     <link rel="stylesheet" href="../css/Styles.css">
-        <!--?php echo $product['foto'];?> --->
 
     </head>
     <body>
+
     <div class="container">
         <?php
 
         $connect = mysqli_connect('localhost', 'root', '', 'deboerlicht');
-        $sql = "SELECT * FROM winkelwagen order by id ASC;";
-        $result = mysqli_query($connect, $sql);
+        $query = "SELECT * FROM winkelwagen order by id ASC;";
+        $result = mysqli_query($connect, $query);
 
 
         if($result):
             if(mysqli_num_rows($result)>0):
                 while($product= mysqli_fetch_assoc($result)):
                     //als je de data base wil na kijken gebruik de command hier onder
-                //print_r($product);
+
                  ?>
 
                 <!--kleine productpagina om te testen of we winkelwagen werkt--->
             <div class="col-sm-4 col-md-3">
                 <form method="post" action="winkelwagenn.php?action=add&id=<?php echo $product['id']; ?>">
                 <div class="products">
-                    <img src=src="../Fotos/Lamp5.png" class="img-responsive"/>
+                    <img src="../Fotos/Lamp5.png" class="img-responsive" alt="kaas"/>
                     <h4 class="text-info"><?php echo $product['naam']; ?></h4>
                     <h4>$ <?php echo $product['prijs'];?></h4>
                     <input type="text" name="hoeveelheid" class="form-control" value="1"/>
@@ -136,12 +146,36 @@ function pre_r($array){
 
         ?>
         <tr>
-            <td><?php echo $product['naam'] ?></td>
-            <td><?php echo $product['hoeveelheid']?></td>
-            <td><?php?></td>
+            <td><?php echo $product['naam'];?></td>
+            <td><?php echo $product['hoeveelheid'];?></td>
+            <td><?php echo $product['prijs'];?></td>
+            <td><?php echo number_format($product['hoeveelheid'] * $product['prijs'], 2);?></td>
+            <td>
+                <a href="winkelwagenn.php?action=delete&id=<?php echo $product['id'];?>">
+                    <div class="btn-danger">Verwijderen</div>
+                </a>
+            </td>
         </tr>
+        <?php
+            $total = $total + ($product['hoeveelheid'] * $product['prijs']);
+            endforeach;
+        ?>
+        <tr>
+            <td colspan="3" align="right">Totaal</td>
+            <td align="right">$ <?php echo number_format($total, 2); ?></td>
+            <td></td>
+        </tr>
+        <tr>
 
-
+            <td colspan="5">
+                <?php
+                if (isset($_SESSION['winkelwagen'])):
+                if (count($_SESSION['winkelwagen']) > 0):
+                ?>
+                <a href="#" class="button">Checkout</a>
+                <?php endif; endif; ?>
+            </td>
+        </tr>
         <?php
          endif;
         ?>
