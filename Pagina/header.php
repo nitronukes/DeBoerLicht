c139e97f26b6e33b7e1349d7a361081d0b2d8536
Timhamer <240159@edu.rocfriesepoort.nl>
2022-06-28 11:50:32 +0200

    Nog een header fix

--- Pagina/header.php
+++ Pagina/header.php
@@ -25,7 +25,6 @@ include("Functions.php");
             <li><a class="headerknop" href="loguit.php">Loguit</a></li>
             <li><a class="headerknop" href="Lampenoverzicht.php?filter=Geencategorie">Lampen</a></li>
             <li><a class="headerknop" href="Bestellingenoverzicht.php">Besteloverzicht</a></li>
-            <li><a class="headerknop" href="Productbeheer.php?deleteID=-1">Productbeheer</a></li>
             <li><a class="headerknop" href="Categoriebeheer.php?deleteID=-1">Categoriebeheer</a></li>
             <li><a class="headerknop" href="index.php">Home</a> </li>
         ';
